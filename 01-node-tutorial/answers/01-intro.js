66540252a0d042933b3db503153d8b4e2289b0cc
johnoro <officialjohnorourke@gmail.com>
2024-10-21 22:05:24 -0400

    completes 02-globals

--- 01-node-tutorial/answers/01-intro.js
+++ 01-node-tutorial/answers/01-intro.js
@@ -1,8 +1,11 @@
-const log = () => {
-	// skips the execution path and this file's path
-	const args = process.argv.slice(2);
+// accepts only one of function args OR CLI args
+const log = (...args) => {
 	if (args.length === 0) {
-		args.push('You can also feed input to this script via the command line.');
+		// skips the execution path and this file's path
+		args = args.concat(process.argv.slice(2));
+		if (args.length === 0) {
+			args.push('You can also feed input to this script via the command line.');
+		}
 	}
 	args.forEach(arg => {
 		console.log(arg);
@@ -10,3 +13,5 @@ const log = () => {
 };
 
 log();
+
+module.exports = log;
