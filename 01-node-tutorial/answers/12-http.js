e43208fa81701a5e7a859745ebf3f14c97ed1e2e
johnoro <officialjohnorourke@gmail.com>
2024-10-22 23:28:57 -0400

    completes 12-http

---
+++ 01-node-tutorial/answers/12-http.js
@@ -0,0 +1,35 @@
+const http = require('node:http');
+
+const buildHtml = (title, text) => {
+	return `
+        <h1>${title}</h1>
+        <p>${text}</p>
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/fake">Missing</a>
+        </nav>
+    `;
+};
+
+const siteMap = {
+	'/': buildHtml('Home', 'Welcome home.'),
+	'/about': buildHtml('About', 'How <i>about</i> this?')
+};
+
+const server = http.createServer((req, res) => {
+	if (req.url in siteMap) {
+		res.writeHead(200);
+		res.end(siteMap[req.url]);
+	} else {
+		res.writeHead(404);
+		res.end(
+			buildHtml('Page not found', `The page you are looking for doesn't exist.`)
+		);
+	}
+});
+
+const port = 3000;
+server.listen(port, () => {
+	console.log(`Web server running on http://localhost:${port}`);
+});
