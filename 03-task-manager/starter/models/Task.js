10babd458444fbb5c8b934f39e61d92d709571cd
johnoro <officialjohnorourke@gmail.com>
2024-12-16 19:09:05 -0500

    empty task model

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -1,14 +1,8 @@
 import mongoose from 'mongoose';
 
 const TaskSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	completed: {
-		type: Boolean,
-		default: false
-	}
+	name: {},
+	completed: {}
 });
 
 export default mongoose.model('Task', TaskSchema);
