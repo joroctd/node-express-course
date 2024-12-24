a45052044227b03f66e557df0b14b0fbccb1f340
johnoro <officialjohnorourke@gmail.com>
2024-12-23 21:37:40 -0500

    removes uneccesary products assignments in controller

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -42,14 +42,14 @@ module.exports = {
 		}
 
 		let products = Product.find(queryObject);
-		products = products.sort(sort ? sort.split(',').join(' ') : 'createdAt');
+		products.sort(sort ? sort.split(',').join(' ') : 'createdAt');
 
 		if (fields) products.select(fields.split(','));
 
 		page = Number(page) || 1;
 		limit = Number(limit) || 10;
 		const skip = (page - 1) * limit;
-		products = products.skip(skip).limit(limit);
+		products.skip(skip).limit(limit);
 
 		products = await products;
 		res.status(200).json({ products, nbHits: products.length });
